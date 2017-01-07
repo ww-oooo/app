d3132b6447cd8bb34f1970815c960f48eee781a2
ww-oooo <1344635839@qq.com>
2017-01-07 08:51:00 +0800

    '1'

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(function(){
+    var lamp = $('.lamp');
+    var nav=$('#nav');
+	var lamp_big = $('.theme li:odd');
+	var theme = $('.theme');
+	var common=$('.theme li:even');
+	var choose=$('.choose');
+	var protect=$('.protected');
+	var choose_two=$('.choose-two');
+	var back=$('.back');
+	var end=$('.main');
+	console.log(protect.length);
+	console.log(lamp_big.length);
+	var flag=true;
+	common.on('touchmove',function(){
+		flag=false;
+	})
+	common.on('touchend',function(){
+		if(flag){
+			if($(this).hasClass('protect')){
+	        	choose_two.css("display", "block");
+	            choose_two.css('transform','translateY(0px)');
+	        }else{
+	        	choose.css("display", "block");
+	            choose.css('transform','translateY(0px)');
+	        }
+			$('.common').css("display", "none");
+			nav.css("display", "block");
+	        end.css("display", "none");
+	        
+	        $(this).next().css("display", "block").addClass('active');
+		}
+		flag=true;
+        return false;
+	});
+//	back.on('touchend',function(){
+//		common.css("display", "block");
+//      nav.css("display", "none");
+//      end.css("display", "block");
+//      
+//      choose.css('transform','translateY(430px)');
+//      choose.css("display", "none");
+//      lamp_big.css("display", "none");
+//	})
+// var scrollw=parseInt($('.microwave').position().left+$('.microwave').width()-$('.theme').width());
+// 
+// 	theme.scroll(function(){
+// 		if(theme.scrollLeft()==scrollw){
+// 			console.log(1);
+// 		}
+// 		if(theme.scrollLeft()==0){
+// 			console.log(2);
+// 		}
+
+   	})
+
+ 
