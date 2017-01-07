d0ed340906b3e44d38e4be3d6fa8367efa1a6ddc
ww-oooo <1344635839@qq.com>
2017-01-07 09:45:26 +0800

    '1'

--- js/index.js
+++ js/index.js
@@ -19,10 +19,10 @@ $(function(){
 		if(flag){
 			if($(this).hasClass('protect')){
 	        	choose_two.css("display", "block");
-	            choose_two.css('transform','translateY(-80px)');
+	            choose_two.css('transform','translateY(-346px)');
 	        }else{
 	        	choose.css("display", "block");
-	            choose.css('transform','translateY(10px)');
+	            choose.css('transform','translateY(30px)');
 	        }
 			$('.common').css("display", "none");
 			nav.css("display", "block");
